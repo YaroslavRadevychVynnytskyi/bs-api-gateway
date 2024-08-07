037185800371b04da620d7a27bb998f0334005ec
Yaroslav <loghitechdf@gmail.com>
2024-08-07 13:21:06 +0300

    Project init

---
+++ src/main/java/com/application/bsapigateway/BsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.application.bsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsApiGatewayApplication.class, args);
+    }
+
+}
